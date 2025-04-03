0a47acf3f1be4b7473c541849fb3cfa6e8d2893b
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-04-02 22:28:26 -0500

    Exercise 2

---
+++ src/FavouriteFood.jsx
@@ -0,0 +1,5 @@
+function FavouriteFood() {
+  return <h1>My favourite food is Pepperoni Pizza!.</h1>
+}
+
+export default FavouriteFood
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import Greeting from "./Greeting.jsx"
+import FavouriteFood from "./FavouriteFood.jsx"
 import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Greeting />
+    <FavouriteFood />
   </StrictMode>
 )
